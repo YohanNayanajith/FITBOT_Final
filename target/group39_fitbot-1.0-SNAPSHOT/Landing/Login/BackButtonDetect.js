e9e8fc54d6a521b2ac79a5bf0985b5013939d77a
YohanNayanajith <yohannayanajith40@gmail.com>
2022-01-29 18:51:26 +0530

    Other Changes

---
+++ target/group39_fitbot-1.0-SNAPSHOT/Landing/Login/BackButtonDetect.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//     window.addEventListener("popstate",showBack());
+// });
+//
+// function showBack(){
+//     window.history.pushState({id:"login_state"},null,"http://localhost:8080/group39_fitbot_war_exploded/login");
+// }
